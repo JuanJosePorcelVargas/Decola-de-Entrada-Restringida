e58330ca496a401b7b062a360444b93769ae7bef
Juan Jose Porcel Vargas <juanjoseporcel11@gmail.com>
2021-10-13 17:10:44 -0400

    Agregar archivos de proyecto.

---
+++ Decola de Entrada  Restringida/Decola.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decola_de_Entrada__Restringida
+{
+    class Decola
+    {
+        static int maxCola = 6;
+        public int primero;
+        public int ultimo;
+        public int[] lista;
+
+
+        public Decola()
+        {
+            primero = 0;
+            ultimo = -1;
+            lista = new int[maxCola];
+        }
+
+        public bool vacia()
+        {
+            if (ultimo < primero)
+                return true;
+            else
+
+                return false;
+
+
+
+
+        }
+        public bool llena()
+        {
+            if (ultimo == maxCola - 1)
+                return true;
+            else
+                return false;
+
+        }
+
+
+
+
+        public void insertar_ultimo(int elemento)
+        {
+            if (llena())
+                MessageBox.Show("La cola esta llena,No puede insertar mas elementos");
+            else
+            {
+                ultimo++;
+                lista[ultimo] = elemento;
+
+
+
+            }
+
+        }
+        public void eliminar_primero()
+        {
+            if (vacia())
+                MessageBox.Show("La cola esta vacia,No puede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("El elemento a eliminar es: " + lista[primero]);
+                for (int i = 0; i <= ultimo - 1; i++)// Para que al eliminar recorra su posicion anterior
+                {
+                    lista[i] = lista[i + 1];
+
+                }
+                ultimo--;
+
+            }
+
+
+
+        }
+        public void eliminar_ultimo()
+        {
+            if (vacia())
+                MessageBox.Show("La cola esta vacia,no puede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("El elemento a eliminar es: " + lista[ultimo]);
+                ultimo--;
+
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Decola de Entrada  Restringida/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decola_de_Entrada__Restringida
+{
+    public partial class Form1 : Form
+
+    {
+        Decola mensajero = new Decola();
+
+        public void mostrarDecola()
+       
+        {
+            listView1.Clear();
+            for (int i = 0; i <= mensajero.ultimo; i++)
+            {
+                listView1.Items.Add(mensajero.lista[i].ToString());
+
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mensajero.insertar_ultimo(Int32.Parse(textBox1.Text));
+            textBox1.Clear();
+            textBox1.Focus();
+            mostrarDecola();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mensajero.eliminar_primero();
+            mostrarDecola();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mensajero.eliminar_ultimo();
+            mostrarDecola();
+        }
+    }
+}
